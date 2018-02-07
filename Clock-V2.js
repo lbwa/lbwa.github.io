54f03f68356d32a4ef2338bca004ef2a56df49b3
Bowie <mr_lbw@outlook.com>
2018-02-08 00:57:43 +0800

    优化细节

--- Clock-V2.js
+++ Clock-V2.js
@@ -1,67 +1,67 @@
 /* jshint esversion: 6 */
-let num = [
-  [0, 1, 2, 3, 4, 5], // 0
-  [1, 2],
-  [0, 1, 6, 4, 3],
-  [0, 1, 6, 2, 3],
-  [5, 6, 1, 2],
-  [0, 5, 6, 2, 3],
-  [0, 5, 4, 3, 2, 6],
-  [0, 1, 2],
-  [0, 1, 2, 3, 4, 5, 6],
-  [0, 5, 6, 1, 2, 3]
-];
+let app = (function() {
+  let num = [
+      [0, 1, 2, 3, 4, 5], // 0
+      [1, 2],
+      [0, 1, 6, 4, 3],
+      [0, 1, 6, 2, 3],
+      [5, 6, 1, 2],
+      [0, 5, 6, 2, 3],
+      [0, 5, 4, 3, 2, 6],
+      [0, 1, 2],
+      [0, 1, 2, 3, 4, 5, 6],
+      [0, 5, 6, 1, 2, 3]
+    ],
+    $ = document.querySelectorAll.bind(document),
+    _hour = $('.hour'),
+    _min = $('.min'),
+    _sec = $('.sec'),
+    _point = $('.point'),
+    time = [_hour[0], _hour[1], _min[0], _min[1], _sec[0], _sec[1]];
 
-let setTime = (digit, nowTime) => {
-  let part = digit.querySelectorAll('.part'),
-    current = parseInt(digit.getAttribute('data-value'));
-  if (!isNaN(current) && current != nowTime) {
-    num[current].forEach((num) => {
-      part[num].classList.remove('on');
-    });
-  }
-  if (isNaN(current) || current != nowTime) {
-    num[nowTime].forEach((num, index) => {
-      part[num].classList.add('on');
-    });
-  }
-  digit.setAttribute('data-value', nowTime);
-};
+  return {
 
-// trigger
-let trigger = (event) => {
-  let _hour = document.querySelectorAll('.hour'),
-    _min = document.querySelectorAll('.min'),
-    _sec = document.querySelectorAll('.sec');
-  setInterval(() => {
-    let now = /(\d)(\d):(\d)(\d):(\d)(\d)/.exec(new Date());
-    setTime(_hour[0], now[1]);
-    setTime(_hour[1], now[2]);
-    setTime(_min[0], now[3]);
-    setTime(_min[1], now[4]);
-    setTime(_sec[0], now[5]);
-    setTime(_sec[1], now[6]);
-    if (now[6] % 2 != 0) {
-      [0, 1, 2, 3].forEach((number) => {
-        document.querySelectorAll('.point')[number].classList.remove('on');
-      });
-    } else {
-      [0, 1, 2, 3].forEach((number) => {
-        document.querySelectorAll('.point')[number].classList.add('on');
-      });
+    setTime: function(nowTime) {
+      let part = this.querySelectorAll('.part'),
+        current = parseInt(this.getAttribute('data-value'));
+      if (!isNaN(current) && current != nowTime) {
+        num[current].forEach((num) => {
+          part[num].classList.remove('on');
+        });
+      }
+      if (isNaN(current) || current != nowTime) {
+        num[nowTime].forEach((num, index) => {
+          part[num].classList.add('on');
+        });
+      }
+      this.setAttribute('data-value', nowTime);
+    },
+
+    runTime: function() {
+      let now = /(\d)(\d):(\d)(\d):(\d)(\d)/.exec(new Date()),
+        setTime = app.setTime;
+      for (let i = 1; i <= 6; i++) {
+        setTime.call(time[i - 1], now[i]);
+      }
+      // 位操作比其他数学操作符和布尔操作符快得多
+      // 偶数的的最低位是0，奇数的最低位是1
+      if (now[6] & 1) {
+        [0, 1, 2, 3].forEach((number) => {
+          _point[number].classList.remove('on');
+        });
+      } else {
+        [0, 1, 2, 3].forEach((number) => {
+          _point[number].classList.add('on');
+        });
+      }
+    },
+
+    trigger: function(event) {
+      // 若多次访问同一对象的方法，则将该方法赋值给局部变量，减少不必要的属性搜索
+      runTime = app.runTime;
+      setInterval(runTime, 1000);
     }
-  }, 1000);
 
-};
-window.addEventListener('load', trigger, false);
-
-// 思路：
-// 关键点：每个数字由七个区域显示
-// 1.建立显示数组
-// 2.设置显示方式
-//  2.1当存在显示的数（即data-value）且与实际要显示的数不同时，去掉小显示区域的opacity:1(即on类)，恢复原状。
-//  2.2当不存在显示的数（初始化）或显示的数值与要显示的数值不同时，给小区域添加on类,并设置data-value，以方便后续判断
-// 3.触发load事件时触发时钟循环函数
-//  3.1取得各个数字的显示区域
-//  3.2取得现在的时间的各个位上的数字
-//  3.3将取得的显示区域和要显示的数字带入2中函数，一直循环下去
+  };
+})();
+window.addEventListener('load', app.trigger, false);
