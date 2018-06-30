5b2450af8070f1b4388d71573e148f11d83d0e96
Bowen <mr_lbw@outlook.com>
2018-06-30 18:03:00 +0800

    chore: extract vendor chunck and build CI config

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,8 @@ module.exports = {
   loading: { color: '#3eaf7c', height: '2px' },
 
   build: {
-    vendor: ['highlight.js'],
+    // 只打包 highlight.js 的核心代码，否则打包而成的 vendor chunk 非常大
+    vendor: ['highlight.js/lib/highlight'],
     extend (config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push(
--- pages/blog/writing/_id/index.vue
+++ pages/blog/writing/_id/index.vue
@@ -23,6 +23,7 @@ export default {
     //
     try {
       const { id } = params
+      console.log('params :', params)
 
       // extract meta info from markdown file
       const { title, date, author, tags, content } = markdownParser(id)
