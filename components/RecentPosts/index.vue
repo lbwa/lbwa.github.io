78331064a09102057c39fa4db28cb0659de30f13
Bowen <mr_lbw@outlook.com>
2018-07-28 09:58:51 +0800

    feat(home-footer): add home footer

---
+++ components/RecentPosts/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    class="home recent-section ta-center flex-js-center flex-ai-center"
+    v-if="hasRecentPosts"
+  >
+    <header class="recent-section-header">
+        <svg class="home-icon icon-bookmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M128 80v380c0 3.3 3.8 5.2 6.4 3.2l116.8-92c2.9-2.1 6.8-2.1 9.6 0l116.8 92c2.6 2 6.4.1 6.4-3.2V80c0-17.7-14.3-32-32-32H160c-17.7 0-32 14.3-32 32z"/>
+      </svg>
+      <h2 class="recent-section-title">Recent Posts</h2>
+    </header>
+    <ul class="recent-posts">
+      <li
+        class="front-teaser no-list-style"
+        v-for="post of recentPosts"
+        :key="post.to"
+      >
+        <router-link
+          :to="`/blog/writings/${post.to}`"
+          class="post-title no-text-decoration"
+        >
+          <h3 class="post-title-content">{{post.title}}</h3>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recentPosts: {
+      type: Array,
+      default () {
+        return {}
+      }
+    }
+  },
+
+  computed: {
+    hasRecentPosts () {
+      return Object.keys(this.recentPosts).length !== 0
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '~/assets/sass/index.sass'
+
+.recent-section
+  border-top: 1px solid $border-white
+  padding: 100px 25px
+
+  +mobile
+    padding: 100px 0
+
+  .recent-section-header
+    margin: 0.83em 0
+    font-style: italic
+
+    .recent-section-title
+      display: inline-block
+      margin: 0
+
+    .icon-bookmark
+      vertical-align: bottom
+      fill: $icon-red
+
+  .recent-posts
+    padding-left: 0
+
+  .front-teaser
+    position: relative
+    margin-top: 1rem
+    margin-bottom: 1rem
+
+    +mobile
+      margin-right: 15px
+      margin-left: 15px
+
+    // box-shadow: 1px 1px 15px 4px rgba(67, 38, 100, 0.15)
+    +shadow-basis(-1, 1)
+
+    &::after
+      border-radius: 5px
+
+  .post-title
+    display: block
+    padding: 1.875rem // 扩充有效点击范围
+
+  .post-title-content
+    display: inline-block
+    margin: 0
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,9 @@
         Positive exploration &amp; deep thinking
       </h4>
       <div :class="['home-navigator', 'animation', animation ? '' : 'ini-ani']">
-        <nuxt-link
+        <router-link
           to="/blog/writings/"
-          class="home-btn button-primary">Writings</nuxt-link>
+          class="home-btn button-primary">Writings</router-link>
         <a
           href="https://github.com/lbwa"
           target="_blank"
@@ -17,33 +17,22 @@
       </div>
     </div>
 
-    <div class="home recent-section ta-center flex-js-center flex-ai-center">
-      <header class="recent-section-header">
-          <svg class="home-icon icon-bookmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <path d="M128 80v380c0 3.3 3.8 5.2 6.4 3.2l116.8-92c2.9-2.1 6.8-2.1 9.6 0l116.8 92c2.6 2 6.4.1 6.4-3.2V80c0-17.7-14.3-32-32-32H160c-17.7 0-32 14.3-32 32z"/>
-        </svg>
-        <h2 class="recent-section-title">Recent Posts</h2>
-      </header>
-      <ul class="recent-posts">
-        <li
-          class="front-teaser no-list-style"
-          v-for="post of recentPosts"
-          :key="post.to"
-        >
-          <router-link
-            :to="`/blog/writings/${post.to}`"
-            class="post-title no-text-decoration"
-          >
-            <h3 class="post-title-content">{{post.title}}</h3>
-          </router-link>
-        </li>
-      </ul>
-    </div>
+    <RecentPosts :recentPosts="recentPosts"/>
+
+    <footer class="home-footer ta-center">
+      <div class="footer-info">Copyright &copy; {{genYear}} <a
+        target="_blank"
+        rel="noopener"
+        href="https://github.com/lbwa"
+        class="author"
+      >Bowen</a></div>
+    </footer>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo'
+import RecentPosts from '~/components/RecentPosts'
 import axios from '~/lib/axios'
 
 export default {
@@ -54,6 +43,13 @@ export default {
       logoRate: 0.2777, // width / height
       animation: false,
       sessionStorage: false
+    }
+  },
+
+  computed: {
+    genYear () {
+      const date = new Date().getFullYear()
+      return date === 2018 ? `2018` : `2018 - ${date}`
     }
   },
 
@@ -78,7 +74,8 @@ export default {
   },
 
   components: {
-    AppLogo
+    AppLogo,
+    RecentPost
   },
 
   head () {
@@ -91,11 +88,16 @@ export default {
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 @import '~/assets/sass/index.sass'
 
 .home
   +flex-box(column)
+
+  /deep/ .home-icon
+    display: inline-block
+    margin-right: 5px
+    width: 1.6rem
 
   &.full-m-height
     min-height: 100vh
@@ -114,51 +116,6 @@ export default {
       margin: 0 .75rem
       transition: all .4s
 
-.recent-section
-  border-top: 1px solid $border-white
-  padding: 100px 25px
-
-  +mobile
-    padding: 100px 0
-
-  .recent-section-header
-    margin: 0.83em 0
-    font-style: italic
-
-    .recent-section-title
-      display: inline-block
-      margin: 0
-
-    .icon-bookmark
-      vertical-align: bottom
-      fill: $icon-red
-
-  .recent-posts
-    padding-left: 0
-
-  .front-teaser
-    position: relative
-    margin-top: 1rem
-    margin-bottom: 1rem
-
-    +mobile
-      margin-right: 15px
-      margin-left: 15px
-
-    // box-shadow: 1px 1px 15px 4px rgba(67, 38, 100, 0.15)
-    +shadow-basis(-1, 1)
-
-    &::after
-      border-radius: 5px
-
-  .post-title
-    display: block
-    padding: 1.875rem // 扩充有效点击范围
-
-  .post-title-content
-    display: inline-block
-    margin: 0
-
 .animation
   transition: opacity 235ms cubic-bezier(.4,0,.2,1), transform 500ms cubic-bezier(.86,0,.07,1)
 
@@ -166,9 +123,26 @@ export default {
     opacity: 0
     transform: translateY(120%)
 
-.home-icon
-  display: inline-block
-  margin-right: 5px
-  width: 1.6rem
+.home-footer
+  padding: 100px 0
+  border-top: 1px solid $border-white
+
+  .footer-info
+    font-size: .8rem
+
+    .author
+      font-weight: bold
+
+@keyframe fadeIn
+  from
+    opacity: 0
+    transform: translate3d(-100%, 0 ,0)
+
+  to
+    opacity: 1
+    transform: none
+
+.fade-in
+  animation-name: fadeIn
 
 </style>
