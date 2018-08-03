cd1370c4bf7cd660d4c3066700b928a428f07619
Bowen <mr_lbw@outlook.com>
2018-08-03 12:12:36 +0800

    feat: define viewport for svg icon

--- components/RecentPosts/index.vue
+++ components/RecentPosts/index.vue
@@ -4,7 +4,7 @@
     v-if="hasRecentPosts"
   >
     <header class="recent-section-header">
-      <svg class="home-icon icon-bookmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <svg width="1.6rem" height="1.6rem" class="home-icon icon-bookmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <path d="M128 80v380c0 3.3 3.8 5.2 6.4 3.2l116.8-92c2.9-2.1 6.8-2.1 9.6 0l116.8 92c2.6 2 6.4.1 6.4-3.2V80c0-17.7-14.3-32-32-32H160c-17.7 0-32 14.3-32 32z"/>
       </svg>
       <h2 class="recent-section-title">Recent Posts</h2>
